5e950e56ca83cee3b155bb4bfc6656f0083c6aa7
maltekliemann <mail@maltekliemann.com>
2022-02-20 15:01:33 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="quintain",
-    version="0.2.1",
+    version="0.2.2",
     author="Forschungs- und Entwicklungszentrum Fachhochschule Kiel GmbH",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
